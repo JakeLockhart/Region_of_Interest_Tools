bfe434bfe7e30402fa19e5a692020505ba627a0d
Lockhart <jsl5865@psu.edu>
2025-12-01 12:23:35 -0500

    Updated Line properties to include preprocessing step of computing the Line ROI Geometry

--- ROI Manager Class/ROIProperties.m
+++ ROI Manager Class/ROIProperties.m
@@ -32,6 +32,7 @@ classdef ROIProperties < dynamicprops & handle
                     Info.Position = ROIobj.Position;
                     Info.LineWidth = ROIobj.LineWidth;
                     Info.CenterPoints = mean(ROIobj.Position);
+                    Info.ROIGeometry = LineGeometry(Info.Position, Info.LineWidth);
 
                 case 'Spline'
                 case 'Polygon'
