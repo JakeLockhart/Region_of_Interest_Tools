eaa20381d141a7f846338836a7ad4dc6dfd39d31
Lockhart <jsl5865@psu.edu>
2025-12-02 15:55:19 -0500

    No longer processes ROI geometry when called. Only requires Line roi offsets to interpolate an ROI intensity profile

--- Utilities/AdjustedImprofile.m
+++ Utilities/AdjustedImprofile.m
@@ -1,4 +1,4 @@
-function ROIProfile = AdjustedImprofile(Image, LineEndPoints, LineWidth)
+function ROIProfile = AdjustedImprofile(Image, ROIManager_Properties_Line_ROIGeometry)
     % <Documentation>
         % AdjustedImprofile()
         %   Samples the pixel intensity values along a user-defined line with adjustable thickness.
@@ -28,28 +28,10 @@ function ROIProfile = AdjustedImprofile(Image, LineEndPoints, LineWidth)
         %   
     % <End Documentation>
 
-    x = LineEndPoints(:,1);
-    y = LineEndPoints(:,2);
-
-    ROI_HalfWidth = round(LineWidth);
-    SamplesOnROI = 1000;
-
-    [X, Y, ~] = improfile(Image, x, y, SamplesOnROI);
-    if isempty(X)
-        ROIProfile = NaN(SamplesOnROI, 1);
-        return
-    end
-
-    Theta = atan2(Y(end) - Y(1), X(end) - X(1));
-    UnitVectorX = -sin(Theta);
-    UnitVectorY = cos(Theta);
-
-    Offsets = -ROI_HalfWidth:ROI_HalfWidth;
-    OffsetX = X + UnitVectorX * Offsets;
-    OffsetY = Y + UnitVectorY * Offsets;
+    OffsetX = ROIManager_Properties_Line_ROIGeometry.OffsetX;
+    OffsetY = ROIManager_Properties_Line_ROIGeometry.OffsetY;
 
     Values = interp2(double(Image), OffsetX, OffsetY, "linear", 0);
-
     ROIProfile = mean(Values, 2, 'omitnan');
 
 end
