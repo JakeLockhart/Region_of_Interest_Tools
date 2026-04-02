3b5880fbe8c8313ea5a39bfbcdbe390e33d2c47a
Jake Lockhart <jsl5865@psu.edu>
2026-04-02 00:18:16 -0400

    Updated how ROIIndex is calculated. Added an ID to each user drawn ROI.

--- ROI Manager Class/ROIConstructor.m
+++ ROI Manager Class/ROIConstructor.m
@@ -24,14 +24,40 @@ classdef ROIConstructor < handle
                 return;
             end
 
-            if isempty(obj.parentClass.ROIMask{ImageIndex})
-                ROIIndex = 1;
-            else
-                ROIIndex = numel(obj.parentClass.ROIMask{ImageIndex}) + 1;
-            end
+            % ROIIndex = numel(obj.parentClass.ROIMask{ImageIndex}) + 1;
+            ROIIndex = obj.getNextROIIndex(ImageIndex, shape);
+            ROIObject.UserData.ID = ROIIdentifier.MakeID(ImageIndex, shape(1), ROIIndex);
 
             obj.windowClass.ROIObjects{ImageIndex}{ROIIndex} = ROIObject;
             obj.parentClass.ROIMask{ImageIndex}{ROIIndex} = createMask(ROIObject);
+% obj.windowClass.ROIObjects{ImageIndex}{end+1} = ROIObject;
+% obj.parentClass.ROIMask{ImageIndex}{end+1} = createMask(ROIObject);
+        end
+    end
+
+    methods(Access = private)
+        function ROIIndex = getNextROIIndex(obj, ImageIndex, shape)
+            ROIList = obj.windowClass.ROIObjects{ImageIndex};
+            shapeInitial = shape(1);
+
+            if isempty(ROIList)
+                ROIIndex = 1;
+                return
+            end
+
+            index = 0;
+            for k = 1:numel(ROIList)
+                ROI = ROIList{k};
+                if isempty(ROI) || ~isfield(ROI.UserData, 'ID')
+                    continue
+                end
+
+                ID = ROI.UserData.ID;
+                if contains(ID, "_"+shapeInitial+"_")
+                    index = index + 1;
+                end
+            end
+            ROIIndex = index + 1;
         end
     end
 
